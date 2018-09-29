db6a001b15b2fc0d0c5d9558466b4a92382841c3
william george <williameverettgeorge@icloud.com>
2018-09-29 17:31:41 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -4,5 +4,5 @@ rain = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
 def display_rainbow(rain) 
   puts "R: #{rain[0]}, O: #{rain[1]}, Y: #{rain[2]}, G: #{rain[3]}, B: #{rain[4]}, I: #{rain[5]}, V: #{rain[6]}"
 end
-display_rainbow(rain)
+
 
